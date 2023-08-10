7bd956b5aa1539ed7fdef18eb82c3bbf33595a65
Lilian Gallon <lilian.gallon@4sh.fr>
2023-08-10 07:42:19 +0200

    feat(project) #5: use toml declared lwjgl version

--- playground/build.gradle.kts
+++ playground/build.gradle.kts
@@ -1,16 +1,11 @@
-import org.apache.tools.ant.taskdefs.condition.Os
 import org.lwjgl.Lwjgl
 import org.lwjgl.Release
-import org.lwjgl.Snapshot
 import org.lwjgl.lwjgl
 
 repositories {
 	// imgui
 	maven("https://raw.githubusercontent.com/kotlin-graphics/mary/master")
 	maven("https://jitpack.io")
-
-	// lwjgl
-	// sonatype()
 }
 
 plugins {
@@ -25,30 +20,13 @@ application {
 
 dependencies {
 	lwjgl {
-		version = Release.`3_3_2`
+		version = Release.valueOf(libs.versions.lwjgl.asProvider().get().replace(".", "_"))
 		implementation(Lwjgl.Preset.everything)
 	}
 
 	implementation(libs.imgui.core)
 	implementation(libs.imgui.gl)
 	implementation(libs.imgui.glfw)
-
-	// val lwjglNatives = if (Os.isFamily(Os.FAMILY_WINDOWS)) {
-	// 	"natives-windows"
-	// } else if (Os.isFamily(Os.FAMILY_UNIX)) {
-	// 	"natives-linux"
-	// } else if (Os.isFamily(Os.FAMILY_MAC)) {
-	// 	"natives-macos"
-	// } else {
-	// 	throw GradleException("Unsupported Operating System")
-	// }
-
-	// configurations.implementation.get().resolvedConfiguration.resolvedArtifacts
-	// 	.forEach {
-	// 		if (it.moduleVersion.id.group == "org.lwjgl") {
-	// 			runtimeOnly("org.lwjgl:${it.moduleVersion.id.name}:${it.moduleVersion.id.version}:$lwjglNatives")
-	// 		}
-	// 	}
 
 	testImplementation(libs.kotlin.test)
 }
